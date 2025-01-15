f65c6d497b6b400dda277bf90a3a5a537a833a53
Sina Salahshour <sina.salahshour.32@gmail.com>
2025-01-15 22:50:43 +0330

    feat: make some changes

--- svelte.config.js
+++ svelte.config.js
@@ -12,15 +12,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		alias: {
 			'styled-system': './styled-system/*'
 		},
@@ -31,7 +23,9 @@ const config = {
 			}
 		}
 	},
-
+	paths: {
+		base: process.env.NODE_ENV === 'production' ? '/cfg-string-matcher' : ''
+	},
 	extensions: ['.svelte', '.svx']
 };
 
